8f5f7bb17e910adcc4e7b8228a855b5ce0f1e3aa
gilettekis <gilettekis@gmail.com>
2022-09-08 18:16:22 +0300

    task 9 first commit

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,76 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+document.getElementById('lbsInput').addEventListener('input',function(e){
+    let lbs= e.target.value;
+    let input = document.getElementById('lbsInput');
+    let data = input.value;
+    document.getElementById('gramsOutput').innerHTML= lbs/0.0022046;
+    document.getElementById('kgOutput').innerHTML= lbs/2.2046;
+    document.getElementById('ozOutput').innerHTML= lbs*16;
+    document.getElementById('output').style.visibility='visible';
+
+    if (data =='') {
+        document.getElementById('output').style.visibility='hidden';
+    }
+});
+function weightConverter(valNum) {
+    document.getElementById("outputKilograms").innerHTML=valNum/2.2046;
+  };
+
+  const form = document.querySelector("form");
+const timeInput = document.getElementById("minutes-input"); 
+const output = document.getElementById("output");
+const fromSelect = document.getElementById("from");
+const toSelect = document.getElementById("to");
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    // console.log('Submitted');
+
+    // const minutes =Number(timeInput.value); 
+    // console.log(minutes);
+
+    // const convertedTime = converter (fromValue, toValue); // +minutes parasymas paverstu stringa i number 
+
+    // const seconds = convertMinutesToSeconds(minutes);
+    
+    const fromValue = fromSelect.value;
+    const toValue = toSelect.value; 
+
+    const convertedTime = converter(fromValue, toValue);
+
+    // console.log(fromSelect.value);
+    // console.log(toSelect.value);
+   
+    output.textContent = convertedTime;
+
+    // console.log(seconds);
+
+});
+const converter = (from,to) => {
+    const time = Number(timeInput.value);
+    if (from === to) {
+        return time; 
+    } else if (from === 'hours' && to ==='minutes') {
+        return time *60;
+    } else if (from === 'hours' && to === 'seconds') {
+        return time *3600;
+    } else if (from ==='minutes' && to === 'hours'){
+        return time /60;
+    } else if (from === 'minutes' && to === 'seconds'){
+        return time *60;
+    } else if (from ==='seconds' && to === 'minutes'){
+        return time /60;
+    } else if (from === 'seconds' && to === 'hours') {
+        return time /3600;
+    }
+
+}
+
+// const convertMinutesToSeconds = (minutes) => { //suskurta funcija convertMinutesToSeconds, argumentas - minutes, grazins sekundes
+// return minutes*60;
+// }
+
+
--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,16 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title
+        this.director = director
+        this.budget = budget
+    }
+}
+    
+let myMovie = new Movie("Harry Potter", "Chris Columbus", 200000000);
+let myMovie2 = new Movie("Anabelle", 'Klaus', 10000000);
+
+
+console.log(myMovie)
