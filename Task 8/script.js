8bc3e33e8ab4624855c924a93c6ac8ce51928dae
gilettekis <gilettekis@gmail.com>
2022-09-08 19:16:01 +0300

    task 8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+const Calculator = (a,b) => {
+this.a = a;
+this.b = b;
+}
+const sum = (a,b) =>{
+return a + b;
+}
+// console.log(sum(4,3));
+
+const subtraction = (a,b) =>{
+    return a-b;
+}
+// console.log(subtraction(5,3));
+
+const multiplication = (a,b) =>{
+    return a*b;
+}
+// console.log(multiplication(2,3));
+
+const division = (a,b)=>{
+    return a/b;
+}
+
+// console.log(division(10,2));
