c148744a6adf147f30a7dae92d7f52d4e58280c6
gilettekis <gilettekis@gmail.com>
2022-09-11 10:51:17 +0300

    task 10

--- Task 10/script.js
+++ Task 10/script.js
@@ -7,6 +7,33 @@ Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pat
     let c = substraction(three, two);
     let d = multiplication(five, two);
 
+    const Calculator = (a,b,c,d) => {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        }
+        const composition = (a,b) =>{
+        return a + b;
+        }
+        console.log(composition(1,4));
+        
+        const substraction = (a,b) =>{
+            return a-b;
+        }
+        console.log(substraction(5,2));
+        
+        const multiplication = (a,b) =>{
+            return a*b;
+        }
+        console.log(multiplication(5,2));
+        
+        const division = (a,b)=>{
+            return a/b;
+        }
+        
+        console.log(division(4,2));
+        
 console.log(a);
 console.log(b);
 console.log(c);
